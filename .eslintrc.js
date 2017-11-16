b9270df3d3dbd10cc34c333cf27999e3a61fd4d8
Matt Dickey <dickey@vermilion.com>
2017-11-15 22:20:22 -0700

    a little more config before starting

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,24 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
+    "comma-dangle": [1,"never"],
+    "no-dupe-args": 1,
+    "no-dupe-keys": 2,
+    "no-empty": 2,
+    "no-extra-parens": [1,"all"],
+    "no-extra-semi": 1,
+    "no-unreachable": 2,
+    "valid-typeof": 1,
+    "no-unexpected-multiline": 1,
+    "dot-notation": 1,
+    "no-multi-spaces": 1,
+    "no-octal": 1,
+    "no-redeclare": 1,
+    "vars-on-top": 1,
+    "no-unused-vars": 1,
+    "no-mixed-spaces-and-tabs": [1,"smart-tabs"],
+    "no-var": 1,
+    "prefer-const": 1,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
