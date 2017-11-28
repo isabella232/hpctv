83adb4d6c3b39e562710bbd80278d41b54a6fcbf
Matt Dickey <dickey@vermilion.com>
2017-11-28 12:10:20 -0700

    fix?

--- src/components/graphs/LineChart.js
+++ src/components/graphs/LineChart.js
@@ -9,7 +9,7 @@ export default {
         datasets: [
           {
             label: 'Data One',
-            backgroundColor: 'teal',
+            backgroundColor: '',
             data: [40, 39, 10, 40, 39, 80, 40]
           }
         ]
