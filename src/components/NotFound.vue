10a88d16c4c81144af404b089eb20487ca963b38
Matt Dickey <dickey@vermilion.com>
2017-11-28 12:14:13 -0700

    temp remove for internal review

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -2,16 +2,16 @@
   <div class="col dead-center not-found">
     <h1>View not found</h1>
     <router-link to="/">Back to Home</router-link>
-    <line-graph></line-graph>
+    <!-- <line-graph></line-graph> -->
   </div>
 </template>
 
 <script>
-import LineGraph from './graphs/LineGraph.vue';
+// import LineGraph from './graphs/LineGraph.vue';
 
 export default {
 
-  components: {LineGraph},
+  // components: {LineGraph},
 
   created() {
     document.body.classList = '';
