34ac9b360bab0241774d7ea3e346fee82716df41
Matt Dickey <dickey@vermilion.com>
2017-12-12 12:20:39 -0700

    Design Tweak to Big Modal

--- src/components/modals-navs/BigModal.vue
+++ src/components/modals-navs/BigModal.vue
@@ -1,55 +1,60 @@
 <template>
-<div class="big-modal">
-  <div class="overlay row dead-center" @click="handleExitTap()">
-    <div class="body row" @click.stop="doNothing()">
-      <header class="col project-title upper">
-        <div class="title">
-          <h1>{{data.name}}</h1>
-          <h2>Area of science</h2>
-        </div>
-        <div class="project-specs col start">
-          <ul class="collection">
-            <div v-for="(stat,i) in data.statData" :key="stat.statName">
-              <stat-card :cardData="data.statData[i]">
-              </stat-card>
-              <hr style="display: block;width: 90%; margin: 5px auto;">
+  <div class="big-modal">
+    <div class="overlay row dead-center" @click="handleExitTap()">
+      <div class="body row" @click.stop="doNothing()">
+        <header class="col project-title upper">
+          <div class="title">
+            <h1>{{data.name}}</h1>
+            <h2>Area of science</h2>
+          </div>
+          <div class="project-specs col start">
+            <ul class="collection">
+              <div v-for="(stat,i) in data.statData" :key="stat.statName">
+                <stat-card :cardData="data.statData[i]">
+                </stat-card>
+                <hr style="display: block;width: 90%; margin: 5px auto;">
+              </div>
+            </ul>
+            <div class="project-lead">
+              <div class="row bottom">
+                <img src="https://placehold.it/100x100" alt="">
+                <div class="col lead-info">
+                  <span class="lime">Lead Name</span>
+                  <span>Lead Job title</span>
+                </div>
+              </div>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatibus eligendi quas voluptatem sint 
+              </p>
             </div>
+          </div>
+        </header>
+
+        <section class="col">
+          <div class="media-container">
+            <img src="/static/img/modal-placeholder.jpg" class="hero-image" alt="">
+          </div>
+          <ul class="media-navigation row center">
+            <button class="inactive" disabled>&bull;</button>
+            <button class="inactive" disabled>&bull;</button>
+            <button class="active">&bull;</button>
+            <button class="new">&bull;</button>
+            <button class="new">&bull;</button>
           </ul>
-          <div class="project-lead">
-            <img src="https://placehold.it/100x100" alt="">
-            <span class="lime">Lead Name</span>
-            <span>Lead Job title</span>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatibus eligendi quas voluptatem sint </p>
-          </div>
-        </div>
-      </header>
 
-      <section class="col">
-        <div class="media-container">
-          <img src="/static/img/modal-placeholder.jpg" class="hero-image" alt="">
-        </div>
-        <ul class="media-navigation row center">
-          <button class="inactive" disabled>&bull;</button>
-          <button class="inactive" disabled>&bull;</button>
-          <button class="active">&bull;</button>
-          <button class="new">&bull;</button>
-          <button class="new">&bull;</button>
-        </ul>
+          <article class="modal-article societal-impact text-center">
+            <h3 class="upper">Societal Impact</h3>
+            <div class="article-content row">
+              <p class="main-content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, voluptas iusto accusamus quia impedit error odit dolores nulla officiis cum esse quidem repellat repudiandae sapiente expedita quibusdam exercitationem, placeat deleniti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum placeat praesentium dignissimos voluptatum facere possimus itaque voluptas mollitia quam omnis minima illo animi iste, rem laboriosam laudantium. Maxime, iure repudiandae! </p>
+            </div>
+          </article>
 
-        <article class="modal-article societal-impact text-center">
-          <h3 class="upper">Societal Impact</h3>
-          <div class="article-content row">
-            <p class="main-content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, voluptas iusto accusamus quia impedit error odit dolores nulla officiis cum esse quidem repellat repudiandae sapiente expedita quibusdam exercitationem, placeat deleniti?
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum placeat praesentium dignissimos voluptatum facere possimus itaque voluptas mollitia quam omnis minima illo animi iste, rem laboriosam laudantium. Maxime, iure repudiandae! </p>
-          </div>
-        </article>
+        </section>
 
-      </section>
-
-      <button class="exit" @click="handleExitTap()"><img src="/static/icon/plus-x-icon.svg" alt="exit"></button>
+        <button class="exit" @click="handleExitTap()"><img src="/static/icon/plus-x-icon.svg" alt="exit"></button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
