de690572df3de28bcf238d994a22ea12a159aee9
Matt Dickey <dickey@vermilion.com>
2017-12-22 16:37:42 -0700

    Working updating chart with animation

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -7,21 +7,80 @@
       <li @click="activeComponent = 'line'">Line Chart</li>
       <li @click="activeComponent = 'three'">Three JS</li>
     </ul>
-    <!-- <line-graph v-if="activeComponent == 'line'"></line-graph> -->
+    <line-chart v-if="activeComponent == 'line'" :cssClasses="'line-graph'" :width="1000" :chartData="chartData" />
   </div>
 </template>
 
 <script>
+import LineChart from './graphs/LineChart.vue';
 export default {
+  components: {
+    LineChart: LineChart
+  },
 
   data() {
     return {
-      activeComponent: 'three'
+      activeComponent: 'line',
+      chartData: {
+        labels: null,
+        datasets: [
+          {
+            label: 'Power',
+            backgroundColor: 'rgba(0,255,255,0.5)',
+            data: [1, 0, 1, 0, 1],
+            pointRadius: 0,
+            borderWidth: 2,
+            type: 'line'
+          }
+        ]
+      }
     };
+  },
+
+  methods: {
+    changeData() {
+      console.log('clicked');
+      const newData = [];
+      for (let i = 0; i < this.chartData.labels.length; i++) {
+        newData[i] = Math.floor(Math.random() * 11);
+      }
+      this.chartData = {
+        labels: this.getArrayofSize(11),
+        datasets: [
+          {
+            label: 'power',
+            backgroundColor: 'rgba(0,255,255,0.5)',
+            data: this.getNextSequence(),
+            pointRadius: 0,
+            borderWidth: 2,
+            type: 'line'
+          }
+        ]
+      };
+    },
+
+    getArrayofSize(num) {
+      return Array.from(new Array(num).keys());
+    },
+
+    getNextSequence() {
+      const currentData = this.chartData.datasets[0].data.slice();
+      currentData.shift();
+      currentData.push(Math.floor(Math.random() * 11));
+      return currentData;
+    }
   },
 
   created() {
     document.body.classList = '';
+    this.chartData.labels = this.getArrayofSize(11);
+    for (let i = 0; i < this.chartData.labels.length; i++) {
+      this.chartData.datasets[0].data[i] = Math.floor(Math.random() * 11);
+    }
+  },
+
+  mounted() {
+    document.getElementById('line-chart').addEventListener('click', this.changeData);
   }
 };
 </script>
---
+++ src/components/graphs/LineChart.vue
@@ -0,0 +1,48 @@
+<script>
+import { Line, mixins } from 'vue-chartjs';
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveProp],
+
+  data() {
+    return {
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: {
+          easing: 'easeInOutQuart'
+        },
+        scales: {
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: 'rgba(60,60,60,1)',
+                lineWidth: 3
+              }
+            }
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: 'rgba(60,60,60,1)',
+                lineWidth: 3
+              }
+            }
+          ]
+        }
+      }
+    };
+  },
+
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
+
+<style src="../../scss/line-graph.scss" lang="scss" scoped />;
