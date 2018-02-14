edeed2fc7a456a4fb85bdc3aaa06c221fc0a7080
Matt Dickey <dickey@vermilion.com>
2018-02-14 16:51:26 -0700

    finished responsive on Projects - main

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,23 +4,39 @@
       <h1>Cheyenne's Featured Projects</h1>
     </header>
 
-    <article class="row dead-center">
-      <div class="case-container">
-        <h2>
-          {{ vuex.headerTileCopy }}
-        </h2>
-      </div>
-      <div class="case-container" v-for="(project, i) in vuex.featuredProjects" :key="project.name">
-        <figure @click="openModal(i)">
-          <img :src="project.heroImage" alt="">
-          <div class="figure-overlay col dead-center">
-            <img class="plus-icon" src="/static/icon/plus-x-icon.svg">
-            <figcaption class="upper">{{ project.name }}</figcaption>
-          </div>
-        </figure>
-      </div>
+    <main class="col dead-center">
+      <div class="wrapper">
 
-    </article>
+        <div class=" row row-1 text-block">
+          <div class="case-container">
+            <h2>
+              {{ vuex.headerTileCopy }}
+            </h2>
+          </div>
+          <div class="case-container" v-if="i < 2" v-for="(project, i) in vuex.featuredProjects" :key="project.name">
+            <figure @click="openModal(i)">
+              <img :src="project.heroImage" alt="">
+              <div class="figure-overlay col dead-center">
+                <img class="plus-icon" src="/static/icon/plus-x-icon.svg">
+                <figcaption class="upper">{{ project.name }}</figcaption>
+              </div>
+            </figure>
+          </div>
+        </div>
+
+        <div class="row row-2">
+          <div class="case-container" v-if="i >= 2" v-for="(project, i) in vuex.featuredProjects" :key="project.name">
+            <figure @click="openModal(i)">
+              <img :src="project.heroImage" alt="">
+              <div class="figure-overlay col dead-center">
+                <img class="plus-icon" src="/static/icon/plus-x-icon.svg">
+                <figcaption class="upper">{{ project.name }}</figcaption>
+              </div>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </main>
     <big-modal v-if="modalIsOpen" :data="modalData" @modalBorderTapped="modalIsOpen = false" />
     <dock-nav />
   </div>
@@ -52,9 +68,9 @@ export default {
 
   methods: {
     /**
-      * Opens a full screen modal and passes the data based on it's position in the array.
-      * @param { Number } i the position in the array. Passed automatically by Vue Template.
-      */
+     * Opens a full screen modal and passes the data based on it's position in the array.
+     * @param { Number } i the position in the array. Passed automatically by Vue Template.
+     */
     openModal(i) {
       this.modalIsOpen = true;
       this.modalData = this.vuex.featuredProjects[i];
