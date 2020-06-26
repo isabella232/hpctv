be99fe275787081544835a98d33c5e0db1d1cb10
Steve Geinosky <stephengeinosky@gmail.com>
2020-06-26 11:16:41 -0600

    Check if data files are in the /public directory.

--- get-sam-data.sh
+++ get-sam-data.sh
@@ -26,6 +26,6 @@ for key in "${!ENDPOINTS[@]}"
 do
     echo "fetching ${ENDPOINTS[$key]}"
     curl --retry 1 --retry-delay 5 --retry-max-time 10 --user ${HPCTV_SAM_API_USER}:${HPCTV_SAM_API_PASSWORD} https://sam.ucar.edu${ENDPOINTS[$key]} -o ./public/data/${key}.json
-    [[ ! -s ./data/${key}.json ]] && echo "./public/data/${key}.json is empty" && exit 1;
+    [[ ! -s ./public/data/${key}.json ]] && echo "./public/data/${key}.json is empty" && exit 1;
     sleep 1
 done
